f472eb1cd3b2356b7cd81da8ea2dd080621e509c
Sidhardh Joe <sidhardhjoe@email.com>
2024-05-21 17:23:42 +0530

    Get Started

--- Navigation.js
+++ Navigation.js
@@ -2,19 +2,34 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import Page1 from './Screen/Page1';
+import Page2 from './Screen/Page2';
 
 const Stack = createNativeStackNavigator();
 
+const LoadFonts=()=>{
+    return useFonts({
+        GelasioRegular:require('./Fonts/static/Gelasio-Regular.ttf'),
+        GelasioBold:require('./Fonts/static/Gelasio-Bold.ttf')
+    })
+}
+
 function TabScreen() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown:false}}>
       <Stack.Screen name="Page1" component={Page1} />
+      <Stack.Screen name='Page2' component={Page2}/>
     </Stack.Navigator>
   );
 }
 
 const Navigation = () => {
+    const [loaded] = LoadFonts();
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
       <TabScreen />
--- Screen/Page1.js
+++ Screen/Page1.js
@@ -1,21 +1,77 @@
 
+import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 const Page1 = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Page1</Text>
-    </View>
-  );
+    const {navigate }=useNavigation();
+    return (
+        <ImageBackground
+            source={require('../Images/Bgimg.png')}
+            style={styles.background}>
+            <StatusBar style="dark" />
+            <Text style={styles.text1}>MAKE YOUR</Text>
+            <Text style={styles.text2}>HOME BEAUTIFUL</Text>
+            <View style={styles.view1}>
+                <Text style={styles.text3}>The best simple place where you discover most wonderful furnitures and make your home beautiful</Text>
+            </View>
+            <TouchableOpacity onPress={()=>navigate('Page2')}>
+            <View style={styles.getstarted}>
+                <Text style={styles.textgetstarted}>Get Started</Text>
+            </View>
+            </TouchableOpacity>
+
+
+
+        </ImageBackground>
+    );
 }
 
 export default Page1;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    background: {
+        flex: 1,
+    },
+    text1: {
+        fontSize: 24,
+        marginTop: "62%",
+        color: "#606060",
+        marginLeft: "8%",
+        fontFamily: "GelasioRegular"
+    },
+    text2: {
+        fontSize: 30,
+        fontFamily: "GelasioBold",
+        marginLeft: "8%",
+    },
+    view1: {
+        width: 300,
+        marginLeft: "15%",
+        marginTop: "5%",
+        fontSize: 24
+    },
+    text3: {
+        fontSize: 18,
+        letterSpacing: 2,
+        color: "#808080"
+    },
+    getstarted: {
+        height: 54,
+        width: 159,
+        backgroundColor: "black",
+        marginTop: "50%",
+        marginLeft: "30%",
+        borderRadius:5
+    },
+    textgetstarted:{
+        color:"white",
+        fontFamily:"GelasioRegular",
+        fontSize:18,
+        textAlign:"center",
+        marginTop:"5%"
+
+    }
 });
