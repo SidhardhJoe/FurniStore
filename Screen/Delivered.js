4ef504d439f4e22e7260880cc3a51366a3d54ed3
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-06-17 10:38:13 +0530

    d1

--- Screen/Delivered.js
+++ Screen/Delivered.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import DeliveredItem from './Component/DeliveredItem'
 
 const Delivered = () => {
   return (
-    <View>
-      <Text>Delivered</Text>
+    <View style={styles.container}>
+     <DeliveredItem/>
+     <DeliveredItem/>
     </View>
   )
 }
 
 export default Delivered
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:"white",
+    flex:1
+  }
+})
