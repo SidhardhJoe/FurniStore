842e141a06204b8e5cbb934161b0c66f6efc7e1b
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-06-18 09:53:27 +0530

    FInal Commit

--- BottomNav.js
+++ BottomNav.js
@@ -1,11 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Ionicons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
 import React from 'react'
 import Home from './Screen/Home';
 import Favourite from './Screen/Favourite';
 import Notification from './Screen/Notification';
 import Profile from './Screen/Profile';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const Bottomtab = createBottomTabNavigator();
@@ -14,24 +13,8 @@ const Bottomtab = createBottomTabNavigator();
 const BottomNav = () => {
     return (
 
-        <Bottomtab.Navigator screenOptions={({ route, Navigation }) => ({
-            tabBarIcon: ({ color, focused, size }) => {
-                let iconName;
-                if (route.name === "Home") {
-                    iconName = focused ? "home" : "home-outline"
-                }
-                else if (route.name === "Favourite") {
-                    iconName = focused ? "bookmark" : "bookmark-outline"
-                }
-                else if (route.name === "Notification") {
-                    iconName = focused ? "notifications" : "notifications-outline"
-                }
-                else if (route.name === "Profile") {
-                    iconName = focused ? "person" : "person-outline"
-                } return <Ionicons name={iconName} size={size} color={color} />
-            }, headerShown: false
-        })}>
-            <Bottomtab.Screen name='Home' component={Home} />
+        <Bottomtab.Navigator screenOptions={{ headerShown: false }}>
+            <Bottomtab.Screen name='Home' component={Home} options={{ tabBarIcon: () =>  <Ionicons name="home" size={14} color="black" /> }} />
             <Bottomtab.Screen name='Favourite' component={Favourite} />
             <Bottomtab.Screen name='Notification' component={Notification} />
             <Bottomtab.Screen name='Profile' component={Profile} />
--- Screen/SearchPage.js
+++ Screen/SearchPage.js
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StatusBar, StyleSheet, Text, TextInput, View, Image } from 'react-native'
+import React, { useState } from 'react'
+import ItemComponent from "./Component/ItemComponent"
+import { TouchableOpacity } from 'react-native';
 
 const SearchPage = () => {
+  const [search, Setsearch] = useState('');
   return (
-    <View>
-      <Text>SearchPage</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <StatusBar></StatusBar>
+      <View style={styles.view12}>
+        <View style={styles.search_bar}>
+          <TextInput style={styles.txtbox}
+            placeholder='Search'
+            value={search}
+            onChangeText={text => Setsearch(text)}
+          />
+        </View>
+        <TouchableOpacity>
+          <Image source={require("../Icons/Search.png")} style={styles.srhicon} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text style={styles.txt1}>Explore new items</Text>
+      </View>
+      <ItemComponent />
+    </ScrollView>
   )
 }
 
 export default SearchPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  search_bar: {
+    height: 50,
+    width: 320,
+    backgroundColor: "white",
+    marginTop: 20,
+    marginLeft: 10,
+    borderRadius: 20,
+    borderWidth: 2
+  },
+  txtbox: {
+    fontSize: 22,
+    marginTop: 8,
+    marginLeft: 10
+  },
+  txt1: {
+    fontSize: 18,
+    marginTop: 10,
+    marginLeft: 15
+  },
+  view12: {
+    flexDirection: "row"
+  },
+  srhicon: {
+    marginTop: 30,
+    marginLeft: 20,
+    height: 30,
+    width: 30
+  }
+})
